990fc2a3174ec8d210030a88f94e5923d01051eb
Nancy Emmanuel <76671907+NancyEmanuel@users.noreply.github.com>
2024-04-01 18:55:34 -0400

    Update BookFlightsAPIControllerTest.java

--- SOFE3980U-ProjectDeliverable1/src/test/java/BookFlightsAPIControllerTest.java
+++ SOFE3980U-ProjectDeliverable1/src/test/java/BookFlightsAPIControllerTest.java
@@ -33,59 +33,17 @@ public class BookFlightsAPIControllerTest {
     private MockMvc mvc;
 
    
+   
     @Test
-    public void Bookdirect() throws Exception {
-        this.mvc.perform(get("/Bookdirect").param("FromAirport","LAX").param("DestinationAirport","TorontoPearson").param("DepartureTime","1:00pm 02-12-2024").param("ArriveTime","4:00pm 02-12-2024")
-        .param("Passengers","1").param("FullName","Jane Doe"))//.andDo(print())
+    public void Bookdirectapi() throws Exception {
+        this.mvc.perform(post("/Bookdirect-api").param("FromAirport","LAX").param("DestinationAirport","TorontoPearson"))//.andDo(print())
        //this.mvc.perform(get("/Bookdirect").param("FromAirport","LAX").param("DestinationAirport","TorontoPearson"))
       
             .andExpect(status().isOk())
-            .andExpect(content().string("Direct flight booked LAXTorontoPearson02-12-202402-12-20241Jane Doe"));
+            .andExpect(content().string("Direct flight booked LAXTorontoPearson"));
     //.andExpect(content().string("LAXTorontoPearson"));
     }
 
-    
-    @Test
-    public void Bookdirect24HourFormat() throws Exception {
-        this.mvc.perform(get("/Bookdirect").param("FromAirport","LAX").param("DestinationAirport","TorontoPearson").param("DepartureTime","13:00 02-12-2024").param("ArriveTime","16:00 02-12-2024")
-        .param("Passengers","1").param("FullName","Jane Doe"))//.andDo(print())
-       //this.mvc.perform(get("/Bookdirect").param("FromAirport","LAX").param("DestinationAirport","TorontoPearson"))
-      
-            .andExpect(status().isOk())
-            .andExpect(content().string("Direct flight booked LAXTorontoPearson02-12-202402-12-20241Jane Doe"));
-    //.andExpect(content().string("LAXTorontoPearson"));
-    }
-
-    @Test
-    public void Bookmultistop() throws Exception {
-        this.mvc.perform(get("/Bookmultistop").param("FromAirport","LAX").param("DestinationAirport","TorontoPearson").param("DepartureTime","1:00pm 02-12-2024").param("ArriveTime","4:00pm 02-12-2024")
-        .param("Stops","JFK").param("Passengers","1").param("FullName","Jane Doe"))//.andDo(print())
-       //this.mvc.perform(get("/Bookmultistop").param("FromAirport","LAX").param("DestinationAirport","TorontoPearson"))
-      
-        .andExpect(status().isOk())
-        .andExpect(content().string("Multistop flight booked LAXTorontoPearson02-12-202402-12-2024JFK1Jane Doe"));
-    //.andExpect(content().string("LAXTorontoPearson"));
-    }
-
-    @Test
-    public void Bookoneway() throws Exception {
-        this.mvc.perform(get("/Bookoneway").param("FromAirport","LAX").param("DestinationAirport","TorontoPearson").param("DepartureTime","1:00pm 02-12-2024").param("ArriveTime","4:00pm 02-12-2024")
-        .param("Passengers","1").param("FullName","Jane Doe"))//.andDo(print())
-       //this.mvc.perform(get("/Bookmultistop").param("FromAirport","LAX").param("DestinationAirport","TorontoPearson"))
-        .andExpect(status().isOk())
-        .andExpect(content().string("Oneway flight booked LAXTorontoPearson02-12-202402-12-20241Jane Doe"));
-    //.andExpect(content().string("LAXTorontoPearson"));
-    }
-
-    @Test
-    public void BookRoundTrip() throws Exception {
-        this.mvc.perform(get("/BookRoundTrip").param("FromAirport","LAX").param("DestinationAirport","TorontoPearson").param("DepartureTime","1:00pm 02-12-2024").param("ArriveTime","4:00pm 02-12-2024")
-        .param("ReturnFromAirport","TorontoPearson").param("ReturnDestinationAirport","LAX").param("ReturnDepartureTime","2-18-2024").param("ReturnArriveTime","2-19-2024").param("Passengers","1").param("FullName","Jane Doe"))//.andDo(print())
-       //this.mvc.perform(get("/Bookmultistop").param("FromAirport","LAX").param("DestinationAirport","TorontoPearson"))
-        .andExpect(status().isOk())
-        .andExpect(content().string("Roundtrip flight booked LAXTorontoPearson02-12-202402-12-2024TorontoPearsonLAX2-18-20242-19-20241Jane Doe"));
-    //.andExpect(content().string("LAXTorontoPearson"));
-    }
 
 
 
